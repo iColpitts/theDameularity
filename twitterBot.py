8db04141545d1f99faad2e39cd2d0ec5bf5238d3
Izzie Colpitts-Campbell <izzieccampbell@gmail.com>
2016-01-23 22:33:12 -0500

    Removed workshop sample code, created retweet bot for retweeting #theDameularity

--- twitterBot.py
+++ twitterBot.py
@@ -1,32 +1,58 @@
-import tweepy, datetime, sys
-import random
-import twitterKeys
- 
-class TwitterAPI:
-    def __init__(self):
-        auth = tweepy.OAuthHandler(twitterKeys.consumer_key, twitterKeys.consumer_secret)
-        auth.set_access_token(twitterKeys.access_token, twitterKeys.access_token_secret)
-        self.api = tweepy.API(auth)
- 
-    def tweet(self, message):
-        self.api.update_status(status=message)
+#twitterTest.py
+import tweepy, twitterKeys, os
+
+# create bot
+auth = tweepy.OAuthHandler(twitterKeys.consumer_key, twitterKeys.consumer_secret)
+auth.set_access_token(twitterKeys.access_token, twitterKeys.access_token_secret)
+api = tweepy.API(auth)
+
+# retrieve last savepoint if available
+try:
+    idFile = open("lastID.txt", "r")
+    lastID = idFile.read()
+    idFile.close()
+except IOError:
+    lastID = ""
+    print "No savepoint found. Trying to get as many results as possible."
 
 
+while True:
 
-firstList = open('pulpNovels.txt','r')
-secondList = open('finish.txt', 'r')
+    # search query
+    timelineIterator = tweepy.Cursor(api.search, q="#theDameularity", since_id=lastID, lang="").items()
 
-first = firstList.readlines()
-second = secondList.readlines()
-first = [n.replace('\n', '') for n in first]
-firstList.close()
-secondList.close()
+    # put everything into a list to be able to sort/filter
+    timeline = []
+    for status in timelineIterator:
+        timeline.append(status)
 
-tweet = random.choice(first) + random.choice(second) + "#theDameularity"   
+    try:
+        lastID = timeline[0].id
+    except IndexError:
+        lastID = lastID
 
-twitter = TwitterAPI()
-twitter.tweet(tweet)
+    tw_counter = 0
+    err_counter = 0
 
+    # iterate the timeline and retweet
+    for status in timeline:
+        try:
+            print "(%(date)s) %(name)s: %(message)s\n" % \
+                { "date" : status.created_at,
+                "name" : status.author.screen_name.encode('utf-8'),
+                "message" : status.text.encode('utf-8') }
 
+            api.retweet(status.id)
+            tw_counter += 1
+        except tweepy.error.TweepError as e:
+            # just in case tweet got deleted in the meantime or already retweeted
+            err_counter += 1
+            #print e
+            continue
+
+    print "Finished. %d Tweets retweeted, %d errors occured." % (tw_counter, err_counter)
+    # write last retweeted tweet id to file
+    with open("lastID.txt", "w") as file:
+        file.write(str(lastID))
 
      
---
+++ twitterTest.py
@@ -0,0 +1,56 @@
+#twitterTest.py
+import tweepy, twitterKeys, os
+
+# create bot
+auth = tweepy.OAuthHandler(twitterKeys.consumer_key, twitterKeys.consumer_secret)
+auth.set_access_token(twitterKeys.access_token, twitterKeys.access_token_secret)
+api = tweepy.API(auth)
+
+# retrieve last savepoint if available
+try:
+	idFile = open("lastID.txt", "r")
+	lastID = idFile.read()
+	idFile.close()
+except IOError:
+	lastID = ""
+	print "No savepoint found. Trying to get as many results as possible."
+
+
+while True:
+
+	# search query
+	timelineIterator = tweepy.Cursor(api.search, q="#theDameularity is retweeting! ", since_id=lastID, lang="").items()
+
+	# put everything into a list to be able to sort/filter
+	timeline = []
+	for status in timelineIterator:
+		timeline.append(status)
+
+	try:
+	    lastID = timeline[0].id
+	except IndexError:
+	    lastID = lastID
+
+	tw_counter = 0
+	err_counter = 0
+
+	# iterate the timeline and retweet
+	for status in timeline:
+		try:
+			print "(%(date)s) %(name)s: %(message)s\n" % \
+				{ "date" : status.created_at,
+				"name" : status.author.screen_name.encode('utf-8'),
+				"message" : status.text.encode('utf-8') }
+
+			api.retweet(status.id)
+			tw_counter += 1
+		except tweepy.error.TweepError as e:
+			# just in case tweet got deleted in the meantime or already retweeted
+			err_counter += 1
+			#print e
+			continue
+
+	print "Finished. %d Tweets retweeted, %d errors occured." % (tw_counter, err_counter)
+	# write last retweeted tweet id to file
+	with open("lastID.txt", "w") as file:
+		file.write(str(lastID))
